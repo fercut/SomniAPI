6c7f7231fb5150e53190dac19944f70f8eecdb38
fercut <ferminycia@gmail.com>
2024-03-16 20:40:35 +0100

    swagger

--- src/openapi/index.js
+++ src/openapi/index.js
@@ -3,31 +3,52 @@ import { readFileSync } from 'fs';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Función para cargar y parsear archivos YAML
-export function parseYaml(file) {
-    const filePath = resolve(__dirname, `./${file}.yml`);
-    return yaml.load(readFileSync(filePath, 'utf8'));
-}
+function parseYaml(file) {
+    return yaml.load(readFileSync(resolve(__dirname, `${file}.yml`), "utf8"));
+  }
 
 // Definición del objeto swaggerDoc
 export const swaggerDoc = {
     openapi: "3.0.0",
     info: {
-        title: "Somni Joyas",
-        description: "Venta de artículos de joyería",
+        title: "API Somni joyas",
+        description: "API for SomnoJoyas DB management",
     },
-    paths: {
-        ...parseYaml('orders'),
-        ...parseYaml('users'),
-        ...parseYaml('articles'),
-    },
+    servers: [
+        {
+            url: 'http://localhost:3000',
+        },
+        {
+            url: 'https://somniapi.onrender.com',
+        },
+    ],
     components: {
         schemas: parseYaml('schemas'),
         securitySchemes: parseYaml('security'),
         examples: parseYaml('examples'),
         responses: parseYaml('responses'),
     },
+    paths: {
+        ...parseYaml('orders'),
+        ...parseYaml('users'),
+        ...parseYaml('articles'),
+        ...parseYaml('misc'),
+    },
+    tags: [
+        {
+            name: 'Orders',
+            description: 'Order-related endpoints',
+        },
+        {
+            name: 'Users',
+            description: 'User-related endpoints',
+        },
+        {
+            name: 'Articles',
+            description: 'Articles-related endpoints',
+        },
+    ]
 };
