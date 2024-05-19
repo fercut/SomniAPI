261bb06db60b651c572e55cc84a37ddc134c9cff
fercut <ferminycia@gmail.com>
2024-05-19 14:45:34 +0200

    usuario root

--- src/services/smtp/sendEmail.js
+++ src/services/smtp/sendEmail.js
@@ -1,18 +1,7 @@
 import nodemailer from 'nodemailer';
 
-const emailQueue = {}; // Objeto para almacenar la última marca de tiempo de envío por dirección de correo electrónico
-
-const sendEmail = async ({ email, subject, message }) => {
-  const currentTime = Date.now();
-  const emailKey = email.toLowerCase(); // Normaliza la dirección de correo electrónico para evitar duplicados
-  
-  // Verifica si la dirección de correo electrónico ha enviado un correo electrónico recientemente
-  if (emailQueue[emailKey] && currentTime - emailQueue[emailKey] < 1800000) { // 1800000 milisegundos = 30 minutos
-    throw new Error('Debes esperar 30 minutos antes de enviar otro correo electrónico.');
-  }
-
-  // Si no hay restricciones, envía el correo electrónico
-  const transporter = nodemailer.createTransport({
+const createTransporter = () => {
+  return nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
@@ -24,18 +13,6 @@ const sendEmail = async ({ email, subject, message }) => {
       rejectUnauthorized: false
     }
   });
-
-  const mailOptions = {
-    from: email,
-    to: 'somnijoyas@gmail.com',
-    subject: email + ' / ' + subject,
-    text: message
-  };
-
-  await transporter.sendMail(mailOptions);
-
-  // Registra la marca de tiempo del envío del correo electrónico
-  emailQueue[emailKey] = currentTime;
 };
 
-export default sendEmail;
+export default createTransporter;
